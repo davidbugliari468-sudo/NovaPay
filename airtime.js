edc2b7c4482a1d62c3b950c65e68917ebc83d1f4
davidbugliari468-sudo <davidbugliari468@gmail.com>
2026-08-03 12:49:16 +0000

    Rix airtime page

--- airtime.js
+++ airtime.js
@@ -3,7 +3,7 @@ NOVAPAY AIRTIME
 Clean Version
 ========================================== */
 
-import { auth, db } from "./firebase-config.js";
+import { auth, db } from "./firebase.js";
 
 import {
     doc,
